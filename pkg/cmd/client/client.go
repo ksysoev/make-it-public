2c068ba80fc9b6bd6417445f076bab0557dbdbf6
Kirill Sysoev <k.sysoev@me.com>
2024-12-26 17:15:15 +0800

    Add configurable flags for server and port in client
    This update introduces `server` and `port` flags to the client command, allowing users to customize the server address and port. The changes improve flexibility and enhance usability for varied deployment scenarios.

--- pkg/cmd/client/client.go
+++ pkg/cmd/client/client.go
@@ -7,18 +7,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type flags struct {
+	server string
+	port   string
+}
+
 func InitCommand() cobra.Command {
-	return cobra.Command{
+	args := flags{}
+
+	cmd := cobra.Command{
 		Use:   "client",
 		Short: "Run client",
 		RunE: func(cmd *cobra.Command, _ []string) error {
-			return RunClientCommand(cmd.Context())
+			return RunClientCommand(cmd.Context(), &args)
 		},
 	}
+
+	cmd.Flags().StringVar(&args.server, "server", "localhost:8081", "server address")
+	cmd.Flags().StringVar(&args.port, "port", "8080", "port to expose")
+
+	return cmd
 }
 
-func RunClientCommand(ctx context.Context) error {
-	client := core.NewClientServer("localhost:8081")
+func RunClientCommand(ctx context.Context, args *flags) error {
+	client := core.NewClientServer(args.server)
 
 	return client.Run(ctx)
 }
